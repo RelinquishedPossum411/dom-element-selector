17a0fd1d7d15f5ba0751948dd3c1f67d3a0f2f83
Benjamin Huang <ben.huang06@gmail.com>
2017-08-01 14:26:38 -0400

    added a "parsed" object that contains all the individual parts in an obj

---
+++ src/core/parsed.js
@@ -0,0 +1,22 @@
+
+
+let parsed = {
+    tag: "",
+    id: "",
+    class: "",
+    attributes: {
+        match: {},
+
+        contains: {},
+
+        matchSpaces: {},
+
+        startsWith: {},
+
+        endsWith: {},
+
+        has: {}
+    }
+};
+
+export default Object.create(parsed);
