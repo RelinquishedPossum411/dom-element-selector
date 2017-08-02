7815b7501e3c44126b5bf88701c6a3066731025f
Benjamin Huang <ben.huang06@gmail.com>
2017-08-02 19:43:36 -0400

    updated set-up for tree

--- src/core/parsed.js
+++ src/core/parsed.js
@@ -1,22 +1,44 @@
 
 
 let parsed = {
+    // An ancestor, but not necessarily a direct ancestor.
+    ancestor: null,
+
+    // A descendant, but not necessarily a direct descendant.
+    descendant: null,
+
+    // An immediate ancestor.
+    parent: null,
+
+    // An immediate descendant.
+    child: null,
     tag: "",
     id: "",
-    class: "",
+    classes: [],
     attributes: {
+        // [attribute=match]
         match: {},
 
+        // [attribute*=match]
         contains: {},
 
+        // [attribute~=match]
         matchSpaces: {},
 
+        // [attribute|=match]
+        matchDashes: {},
+
+        // [attribute^=match]
         startsWith: {},
 
+        // [attribute$=match]
         endsWith: {},
 
-        has: {}
+        // [attribute]
+        has: []
     }
 };
 
-export default Object.create(parsed);
+export default function () {
+    return Object.create(parsed);
+};
