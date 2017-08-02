7e8fdf12beabf201fdf79493141a60cc9dee5181
Benjamin Huang <ben.huang06@gmail.com>
2017-08-01 21:32:14 -0400

    updated handling of '#'

--- src/core/selector.js
+++ src/core/selector.js
@@ -20,8 +20,13 @@ export default function selector(string) {
         // type selector
         // id selector
         if (current === "#") {
-            component = getSubstring(string.substring(cursor + 1)).sub[0];
-            console.log("here! " + component);
+            let substring = getSubstring(string.substring(cursor + 1));
+            component = substring.sub[0];
+
+            // Compensate for lost merging characters.
+            cursor += component.length + substring.mergedFirst;
+
+            console.log("here! " + component + "\n" + substring.mergedFirst);
         }
 
         // class selector
@@ -37,12 +42,16 @@ export default function selector(string) {
      */
     function getSubstring(string) {
         let strIndex = -1,
+            // This will keep track of how many times the first item got merged.
+            mergedFirst = 0,
             splitted = string.split(regex.rSpecialCharacters);
 
         for (let i = 0; i < splitted.length; i++) {
             strIndex += splitted[i].length + 1;
 
-            if (i == 0)
+            console.log(splitted);
+
+            if (i === 0)
                 continue;
 
             // Check previous entry, to see if the delimiting character was
@@ -52,6 +61,9 @@ export default function selector(string) {
                 if (string[strIndex - splitted[i].length] === " ")
                     splitted[i - 1] = splitted[i - 1].substring(0, splitted[i - 1].length - 1);
                 else {
+                    if (i - 1 === 0)
+                        mergedFirst++;
+
                     splitted.splice(i - 1, 2, splitted[i - 1].substring(0, splitted[i - 1].length - 1) + string[strIndex - splitted[i].length - 1] + splitted[i]);
                     i--;
                 }
@@ -62,7 +74,8 @@ export default function selector(string) {
 
         return {
             sub: splitted,
-            length: splitted.length
+            length: splitted.length,
+            mergedFirst: mergedFirst
         };
     }
 
--- src/util/regex.js
+++ src/util/regex.js
@@ -1,4 +1,4 @@
 
-const rSpecialCharacters = /[!"#$%&'()*+,./:;<=>?@[\]^`{|}~]/;
+const rSpecialCharacters = /[!"#$%&'()*+,./:;<=>?@[\]^`{|}~ ]/;
 
 export { rSpecialCharacters };
