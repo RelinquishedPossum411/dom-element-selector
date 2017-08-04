e311be39152e527ce7ebb768132e3ae59d3a6ef9
Benjamin Huang <ben.huang06@gmail.com>
2017-08-04 00:28:22 -0400

    grouper function for splitting the work

---
+++ src/core/grouper.js
@@ -0,0 +1,14 @@
+
+import selector from "./selector";
+import splitter from "./splitter";
+import tidyer from "../util/delimiterClean";
+import * as regex from "../util/regex";
+
+/**
+ * Divides the initial string into groups so that they can be parsed
+ * individually.
+ */
+
+ export default function grouper(string) {
+
+ }
