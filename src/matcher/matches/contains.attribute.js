45f6865038abb7698d7644879125b48f3e12062a
Benjamin Huang <ben.huang06@gmail.com>
2017-08-10 12:24:42 -0400

    Attr Matcher: contains substring

---
+++ src/matcher/matches/contains.attribute.js
@@ -0,0 +1,13 @@
+
+import { isSubsetObject as check } from "../util/isSubset";
+
+/**
+ * contains.attribute.js
+ * [attribute*=value]
+ */
+export default function (element, attributeContainsObject) {
+    // Use the subset checker tool.
+    return check(attributeContainsObject, element.attributes, (a, b, c, d) => {
+        return d.name === a && d.value.indexOf(b) !== -1;
+    });
+}
