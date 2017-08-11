3b54b24dcbfff9cc345c9df66614f1cfab33cdaa
Benjamin Huang <ben.huang06@gmail.com>
2017-08-10 22:30:05 -0400

    searcher function creates a search list for recursive function

--- src/matcher/searcher.js
+++ src/matcher/searcher.js
@@ -1,11 +1,92 @@
 
 import global from "../util/globals";
+import isEmptyObject from "../util/tools/isEmptyObject";
+import matches from "./matches/matches";
 
 /**
  * Searches for
  */
-export default function (root, tree) {
+export default function searcher(root, search, selected) {
     for (const child of root.children) {
-        // Check the unique properties first: ID, tag name and classes.
+        // Only check the properties that are in the search criteria.
+        // Ignore them if they are omitted; assume they match.
+        let str,
+            matched = 0;
+
+        console.log("Checking " + child);
+        console.log("Tree: ");
+        console.log(search);
+
+        const routine = buildSearchRoutine(search);
+
+        console.log("Routine:");
+        console.log(routine);
+
+        for (const instruction of routine) {
+            console.log("Checking instruction " + instruction + " with " + child);
+            console.log(instruction);
+            console.log(child);
+
+            str = "match" + instruction.formatted;
+
+            if (matches[str] && matches[str](child, instruction.value))
+                matched++;
+        }
+
+        // Add to selected.
+        if (matched === routine.length)
+            selected.push(child);
+
+        // Recurse to search all children.
+        searcher(child, search, selected);
     }
+
+    return selected;
+}
+
+/**
+ * Breaks down a search tree to the individual criteria to search for in
+ * the DOM.
+ */
+function buildSearchRoutine(search) {
+    let searches = [];
+
+    for (const term of Object.keys(search)) {
+        /*console.log("Term " + term);
+        console.log(search[term]);
+        console.log(!!search[term]);*/
+        if (!search[term] || isEmptyObject(search[term]))
+            continue;
+
+        if (Object.prototype.toString.call(search[term]) === "[object Object]" &&
+            term === "attributes") {
+            for (const a of Object.keys(search[term])) {
+                if (!search[term][a] || isEmptyObject(search[term][a]))
+                    continue;
+
+                searches.push({
+                    depth: search[term],
+                    property: a,
+                    value: search[term][a],
+                    formatted: format(term) + format(a)
+                });
+            }
+        } else {
+            searches.push({
+                depth: search,
+                property: term,
+                value: search[term],
+                formatted: format(term)
+            });
+        }
+    }
+
+    return searches;
+}
+
+/**
+ * Formats an attribute string so that we can work with it.
+ */
+function format(string) {
+    return string.split(/[-_]+/).map(v => v ? v.charAt(0).toUpperCase() + (v.length > 1 ? v.substring(1) : "") : "").join("");
 }
--- src/util/tools/isEmptyObject.js
+++ src/util/tools/isEmptyObject.js
@@ -1,4 +1,5 @@
 
 export default function (object) {
-    return object.constructor === [].constructor && Object.keys(object).length === 0;
+    return  (object.constructor === [].constructor || object.constructor === {}.constructor) &&
+            Object.keys(object).length === 0;
 }
