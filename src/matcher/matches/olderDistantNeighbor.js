a2c13c7a834901d41721026776454346f9c0e1f9
RelinquishedPossum411 <ben.huang06@gmail.com>
2017-08-14 19:34:36 -0400

    Matcher Core: older distant neighbor

---
+++ src/matcher/matches/olderDistantNeighbor.js
@@ -0,0 +1,17 @@
+
+import searcher from "../core/searcher";
+
+export default function (element, olderDistantNeighborMatch) {
+	let olderNeighbor;
+
+	if (element.previousElementSibling) {
+		olderNeighbor = element.previousElementSibling;
+
+		while (olderNeighbor) {
+			if (searcher(olderNeighbor, olderDistantNeighborMatch))
+				return olderNeighbor;
+		}
+	}
+
+	return false;
+}
