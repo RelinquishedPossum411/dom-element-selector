fc299cbfbc11cc34485ea53edf8399cfe7f8e30f
RelinquishedPossum411 <ben.huang06@gmail.com>
2017-08-28 13:56:49 -0400

    Pseudo core: recursive nthChild matcher

--- src/pseudoClass/matches/nthChild.js
+++ src/pseudoClass/matches/nthChild.js
@@ -1,2 +1,25 @@
 
-export default function (element) {}
+import firstChild from "./firstChild";
+
+/**
+ * :nth-Child
+ * A recursive approach the evaluating the place of an element among its
+ * children.
+ *
+ * @param element - the element passed in by the matcher.
+ * @param nth - the first parameter specified in the selector string.
+ *
+ * @return returns whether an element is n - 1 elements from the first element.
+ */
+export default function nthChild(element, nth) {
+	if (typeof nth !== "number")
+		nth = Number.parseInt(nth);
+
+	if (nth <= 0)
+		nth = 1;
+
+	if (nth === 1)
+		return firstChild(element);
+
+	return element.previousElementSibling ? nthChild(element.previousElementSibling, nth - 1) : false;
+}
