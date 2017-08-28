bbedd25a5d06fa44f4f3a079e553188dfa2e43c5
RelinquishedPossum411 <ben.huang06@gmail.com>
2017-08-27 23:54:09 -0400

    Pseudo core: last child add to matches list

--- src/pseudoClass/matches/matches.js
+++ src/pseudoClass/matches/matches.js
@@ -1,10 +1,12 @@
 
 import pseudoFirstChild from "./firstChild";
+import pseudoLastChild from "./lastChild";
 
 // Each pseudo-class function receives two parameters:
 //  1. the element being checked.
 //  2. the value of any parameters of the pseudo-class, like nth-child(n).
 // So the value, n, is passed in. If there is no parameter, pass in undefined.
 export default {
-    firstChild: pseudoFirstChild
+    firstChild: pseudoFirstChild,
+	lastChild: pseudoLastChild
 };
