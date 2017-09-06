e08a7f5d0cc4d7e85adffffe1079205c6e8ffebf
RelinquishedPossum411 <ben.huang06@gmail.com>
2017-09-05 22:06:05 -0400

    element wrapper iterable

--- src/core/elementCollection.js
+++ src/core/elementCollection.js
@@ -2,5 +2,20 @@
 import matcher from "../matcher/matcher";
 
 export default class {
+	constructor(selector) {
+		this.elements = matcher(selector);
+	}
 
+	[Symbol.iterator]() {
+		let step = 0;
+
+		return {
+			next() {
+				if (step >= this.elements.length)
+					return { done: true };
+
+				return { value: this.elements[step++] };
+			}
+		};
+	}
 }
